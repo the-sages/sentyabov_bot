79b9565f5d4aa4ef141abab07610b4119cfeb6cf
Anton Krichevskii <mrskadi0@gmail.com>
2017-08-18 11:11:55 +0300

    Go to basic node-tg-bot-api module

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.use(require('body-parser').json());
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log('Process listening on port ' + server.address().port);
+});
+
+app.get('/', (req, res) => {
+    res.send('I\'m Sentyabov!');
+});
+
+module.exports = function (bot) {
+    app.post('/' + bot.token, function (req, res) {
+        bot.processUpdate(req.body);
+        res.sendStatus(200);
+    });
+};
